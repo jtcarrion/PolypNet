19d88b09b86ab4ba719521bfcecdf8aee66b3dcc
Jackson Carrion <57613878+jtcarrion@users.noreply.github.com>
2022-11-27 01:03:58 -0700

    Finished the mask_up( ) functionality

--- LoadModel.py
+++ LoadModel.py
@@ -12,9 +12,17 @@ import numpy as np
 import torchvision
 from torchvision.io import read_image
 import torchvision.transforms as T
+from PIL import Image, ImageDraw, ImageFilter
 
 print(os.getcwd())
 os.chdir('C:/Users/16023/Desktop/BMI_540_Project')
+
+img = ['./OG_test/55.tif', './OG_test/37.tif']
+mask = ['./GT_test/55.tif', './GT_test/37.tif']
+
+data = pd.DataFrame(columns=[['img_path'], ['mask_path']])
+data['img_path'] = img
+data['mask_path'] = mask
 
 img_size = (288, 384, 3)
 num_classes = 1
@@ -23,29 +31,22 @@ class_names = ['background', 'polyp']
 class_rgb_values = [[0, 0, 0], [255, 255, 255]]
 
 
-# helper function for data visualization
-def visualize(**images):
-    """
-    Plot images in one row
-    """
-    n_images = len(images)
-    plt.figure(figsize=(20, 8))
-    for idx, (name, image) in enumerate(images.items()):
-        plt.subplot(1, n_images, idx + 1)
-        plt.xticks([]);
-        plt.yticks([])
-        # get title from the parameter names
-        plt.title(name.replace('_', ' ').title(), fontsize=20)
-        plt.imshow(image)
-    plt.show()
+def mask_up(x, y):
+    im1 = Image.open(x)
+    im2 = Image.open(y)
+
+    m = Image.new("L", im1.size, 128)
+    im = Image.composite(im1, im2, m)
+    return im
 
 
-def get_validation_augmentation():
-    # Add sufficient padding to ensure image is divisible by 32
-    test_transform = [
-        album.PadIfNeeded(min_height=288, min_width=384, always_apply=True, border_mode=0),
-    ]
-    return album.Compose(test_transform)
+def get_preprocessing(preprocessing_fn=None):
+    _transform = []
+    if preprocessing_fn:
+        _transform.append(album.Lambda(image=preprocessing_fn))
+    _transform.append(album.Lambda(image=to_tensor, mask=to_tensor))
+
+    return album.Compose(_transform)
 
 
 def to_tensor(x, **kwargs):
@@ -72,7 +73,8 @@ class EndoscopyPrediction(torch.utils.data.Dataset):
             augmentation=None,
             preprocessing=None,
     ):
-        self.image_paths = self
+        self.image_paths = data['img_path'].values.tolist()
+        self.mask_paths = data['mask_path'].values.tolist()
         self.augmentation = augmentation
         self.preprocessing = preprocessing
 
@@ -80,26 +82,23 @@ class EndoscopyPrediction(torch.utils.data.Dataset):
 
         # read images and masks
         image1 = cv2.cvtColor(cv2.imread(self.image_paths[i]), cv2.COLOR_BGR2RGB)
-       # mask = cv2.cvtColor(cv2.imread(self.mask_paths[i]), cv2.COLOR_BGR2RGB)
-
-        # one-hot-encode the mask
-        #mask = one_hot_encode(mask, self.class_rgb_values).astype('float')
+        mask1 = cv2.cvtColor(cv2.imread(self.mask_paths[i]), cv2.COLOR_BGR2RGB)
 
         # apply augmentations
         if self.augmentation:
-            sample = self.augmentation(image=image1, mask=image1)
-            image1, mask = sample['image'], sample['mask']
+            sample = self.augmentation(image=image1, mask=mask1)
+            image1, mask1 = sample['image'], sample['mask']
 
         # apply preprocessing
         if self.preprocessing:
-            sample = self.preprocessing(image=image1, mask=image1)
-            image1, mask = sample['image'], sample['mask']
+            sample = self.preprocessing(image=image1, mask=mask1)
+            image1, mask1 = sample['image'], sample['mask']
 
-        return image1
+        return image1, mask1
 
     def __len__(self):
         # return length of
-        return len(self.image_paths)
+        return len(self.image_path)
 
 
 # Center crop padded image / mask to original image dims
@@ -120,46 +119,64 @@ model = smp.Unet(
     activation=ACTIVATION,
 )
 
-#preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
 
 # load best saved model checkpoint from the current run
 if os.path.exists('./UNet.pth'):
     model = torch.load('./UNet.pth', map_location=torch.device('cpu'))
     print('Loaded UNet model from this run.')
 
+x = r'./OG_test/55_test.png'
+y = r'./GT_test/55_test.png'
+z = mask_up(x,y)
+z.show()
+
+
+'''
+im1 = Image.open(r'./OG_test/55_test.png')
+im2 = Image.open(r'./GT_test/55_test.png')
+
+mask = Image.new("L", im1.size, 128)
+im = Image.composite(im1, im2, mask)
 test_transform = [album.PadIfNeeded(min_height=288, min_width=384, always_apply=True, border_mode=0)]
 
 f = album.Compose(test_transform)
 
 #print(model.eval())
 
-img = './OG_test/55_test.png'
+#img = Image.open(img)
+#x1 = torch.tensor(img, dtype=torch.PngImageFile).to(torch.device("cpu")).unsqueeze(0)
+#x1 = x1[:3]
 
-#image = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)
-#lol = f(image=image)
+#image = cv2.cvtColor(cv2.imread(r"./OG_test/55.tif"), cv2.COLOR_BGR2RGB)
+#print(image.shape)
+#print(image[0:1].shape)
+
+#lol = f(image=image[0:1])
 #x = lol['image']
 #y = list(x)
 #z = np.array(y)
+#print(z.shape)
 
-#test_img = EndoscopyPrediction(img)
+#x_tensor = torch.from_numpy(image).to(torch.device("cpu")).unsqueeze(0)
+
+#test_img = EndoscopyPrediction(data,
+#                               augmentation=get_validation_augmentation(),
+#                               preprocessing=get_preprocessing(preprocessing_fn),
+#                               )
 #lol = test_img[0]
 
-
-lol = read_image(img)
-pad = nn.ZeroPad2d(1)
-
+#lol = read_image(img)
+#print(lol.shape)
+#pad = nn.ZeroPad2d((0,1,1,0))
 # pad the input tensor
-output = pad(lol)
-torch.reshape(output, (288, 384))
-#x_tensor = torch.from_numpy(lol[:,:]).to(torch.device("cpu")).unsqueeze(0)
+#output = pad(lol)
+#print(output.shape)
 
-pred_mask = model(output)
-pred_mask = pred_mask.detach().squeeze().cpu().numpy()
+#pred_mask = model(lol)
+#pred_mask = pred_mask.detach().squeeze().cpu().numpy()
 # Convert pred_mask from `CHW` format to `HWC` format
-pred_mask = np.transpose(pred_mask,(1,2,0))
+#pred_mask = np.transpose(pred_mask,(1,2,0))
 
 
-visualize(
-        original_image = img,
-        predicted_mask = pred_mask
-    )
+'''
